c57d2f84f62df65d5696e00d9e88352ef0051c8b
gowatana <gowatana@vmtn.jp>
2018-11-02 12:39:28 +0900

    Update setup-02-02_create-esxi-vm.ps1

--- setup-02-02_create-esxi-vm.ps1
+++ setup-02-02_create-esxi-vm.ps1
@@ -1,5 +1,3 @@
-
-
 $vm_name = "vm-esxi-template-01"
 $hv_name = "192.168.1.20"
 
@@ -17,12 +15,7 @@ $vm = New-VM -Name $vm_name -VMHost $hv_name `
     -DiskGB $vmdk_gb -Datastore $ds_name -StorageFormat Thin `
     -NetworkName $pg_name
 
-#$vm | Get-NetworkAdapter | Set-NetworkAdapter -NetworkName $pg_name -StartConnected:$true -Confirm:$false
-
 $vm = Get-VM -Name $vm_name
 $vm_config_spec = New-Object VMware.Vim.VirtualMachineConfigSpec
 $vm_config_spec.NestedHVEnabled = $true
 $vm.ExtensionData.ReconfigVM($vm_config_spec)
-
-# $iso_path = "[datastore1] iso/ESXi-6.7.0-20181004001-standard.iso"
-# $vm | New-CDDrive -IsoPath $iso_path -StartConnected:$true
