82b92f374e13f0437a69d05d568241433f71e455
Go Watanabe <gowatana@vmtn.jp>
2019-11-24 00:42:45 +0900

    move script parts

--- setup_vSAN-Cluster_Hybrid.ps1
+++ setup_vSAN-Cluster_Hybrid.ps1
@@ -14,14 +14,14 @@ $global:DefaultVIServers | % {
 Connect-VIServer -Server $base_vc_address `
     -User $base_vc_user -Password $base_vc_pass -Force |
     select Name,Version,Build,IsConnected | Format-List
-./setup-02-04_clone-vsan-vms.ps1 $config_file_name
-./setup-02-05_config-vsan-vms.ps1 $config_file_name
+./parts/setup-02-04_clone-vsan-vms.ps1 $config_file_name
+./parts/setup-02-05_config-vsan-vms.ps1 $config_file_name
 Disconnect-VIServer -Server $base_vc_address -Confirm:$false
 
 Connect-VIServer -Server $nest_vc_address `
     -User $nest_vc_user -Password $nest_vc_pass -Force |
     select Name,Version,Build,IsConnected | Format-List
-./setup-03-01_create-vsan-cluster.ps1 $config_file_name
-./setup-03-01a_setup-vsan-disk_hybrid.ps1 $config_file_name
-./setup-03-02_setup-vsan-cluster.ps1 $config_file_name
+./parts/setup-03-01_create-vsan-cluster.ps1 $config_file_name
+./parts/setup-03-01a_setup-vsan-disk_hybrid.ps1 $config_file_name
+./parts/setup-03-02_setup-vsan-cluster.ps1 $config_file_name
 Disconnect-VIServer -Server $nest_vc_address -Confirm:$false
