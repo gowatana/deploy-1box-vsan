affc59f1bba7da4ba1a3c4fe95eb60552429d93f
Go Watanabe <gowatana@vmtn.jp>
2019-12-31 23:34:02 +0900

    move files

--- setup_vSAN-Witness-Host.ps1
+++ setup_vSAN-Witness-Host.ps1
@@ -16,7 +16,7 @@ task_message "Witness-1_Start" "Setup Witness-Host VA"
 Connect-VIServer -Server $base_vc_address `
     -User $base_vc_user -Password $base_vc_pass -Force |
     select Name,Version,Build,IsConnected | Format-List
-./Witness/parts/step-1_clone-vSAN-Witness-VA.ps1
+./parts/Witness/step-1_clone-vSAN-Witness-VA.ps1
 
 task_message "Witness-1_End" "Disconnect from All vCeners"
 disconnect_all_vc
@@ -25,7 +25,7 @@ task_message "Witness-2_Start" "Setup Witness-Host Guest"
 Connect-VIServer -Server $base_vc_address `
     -User $base_vc_user -Password $base_vc_pass -Force |
     select Name,Version,Build,IsConnected | Format-List
-./Witness/parts/step-2_config-vSAN-Witness-VA-Guest.ps1
+./parts/Witness/step-2_config-vSAN-Witness-VA-Guest.ps1
 
 task_message "Witness-2_End" "Disconnect from All vCeners"
 disconnect_all_vc
@@ -34,7 +34,7 @@ task_message "Witness-3_Start" "Setup Witness-Host on vCenter"
 Connect-VIServer -Server $nest_vc_address `
     -User $nest_vc_user -Password $nest_vc_pass -Force |
     select Name,Version,Build,IsConnected | Format-List
-./Witness/parts/step-3_add-vSAN-Witness-Host-WTS.ps1
+./parts/Witness/step-3_add-vSAN-Witness-Host-WTS.ps1
 
 task_message "Witness-3_End" ("Disconnect from All vCeners")
 disconnect_all_vc
