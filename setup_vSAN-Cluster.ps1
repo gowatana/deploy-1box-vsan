e529452c90d668400eaf1a52e627cc1c945d1410
Go Watanabe <gowatana@vmtn.jp>
2020-01-05 10:52:49 +0900

    Merge AddDG to set_vSAN-Cluster.ps1

--- setup_vSAN-Cluster.ps1
+++ setup_vSAN-Cluster.ps1
@@ -96,6 +96,25 @@ if($create_vsan_cluster -eq $true){
     disconnect_all_vc
 }
 
+if($vsan_dg_count){
+    for($i=2; $i -le $vsan_dg_count; $i++){
+        task_message "AddDG-01_Start" "Add VMDK for vSAN Disk Group"
+        connect_vc -vc_addr $base_vc_address -vc_user $base_vc_user -vc_pass $base_vc_pass
+        ./parts/step_5-1_add-vsan-vmdk.ps1
+
+        task_message "AddDG-01_End" "Add VMDK for vSAN Disk Group"
+        disconnect_all_vc
+
+        task_message "AddDG-02_Start" "Add vSAN Disk Group"
+        connect_vc -vc_addr $nest_vc_address -vc_user $nest_vc_user -vc_pass $nest_vc_pass
+        ./parts/step_5-2_fix-disk-type.ps1
+        ./parts/step_5-3_add-vsan-dg.ps1
+
+        task_message "AddDG-02_End" "Add vSAN Disk Group"
+        disconnect_all_vc
+    }
+}
+
 if($create_vsan_2node -eq $true){
     task_message "Main-05_Start" "Setup 2-Node vSAN"
     connect_vc -vc_addr $nest_vc_address -vc_user $nest_vc_user -vc_pass $nest_vc_pass
