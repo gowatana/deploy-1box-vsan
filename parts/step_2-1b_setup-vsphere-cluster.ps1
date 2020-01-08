9e5c961a532d1bef707d0f07da5707b139f21267
Go Watanabe <gowatana@vmtn.jp>
2020-01-09 02:01:04 +0900

    remove Empty Discovered virtual machine Folder

--- parts/step_2-1b_setup-vsphere-cluster.ps1
+++ parts/step_2-1b_setup-vsphere-cluster.ps1
@@ -21,3 +21,7 @@ task_message "02-01b_03" "Set UserVars.SuppressShellWarning = 1"
 $cluster | Get-VMHost | Get-AdvancedSetting -Name "UserVars.SuppressShellWarning" |
     Set-AdvancedSetting -Value 1 -Confirm:$false |
     select Entity,Name,Value | Sort-Object Entity | ft -AutoSize
+
+task_message "02-01_04" "Remove VM Folder: Discovered virtual machine"
+Get-Folder -Type VM -Name "Discovered virtual machine" |
+    where {($_ | Get-VM).Count -eq 0} | Remove-Folder -Confirm:$false
