38e5e5197495b9c130eb47662347417c73479d03
Go Watanabe <gowatana@vmtn.jp>
2020-04-14 10:49:55 +0900

    Add Test-VsanClusterHealth workaround

--- parts/step_20-1_test-vsan-cluster.ps1
+++ parts/step_20-1_test-vsan-cluster.ps1
@@ -1,8 +1,12 @@
 $cluster = Get-Cluster -Name $nest_cluster_name
 
 task_message "20-01-01" "Test vSAN Health"
-Get-Cluster $nest_cluster_name | Test-VsanClusterHealth |
-    select Cluster,TimeOfTest,OverallHealthStatus,OverallHealthDescription | fl
+if((Get-Command Test-VsanClusterHealth).Version -eq "12.0.0.15939648"){
+    "Skip - Test-VsanClusterHealth issue workaround for PowerCLI 12"
+}else{
+    Get-Cluster $nest_cluster_name | Test-VsanClusterHealth |
+        select Cluster,TimeOfTest,OverallHealthStatus,OverallHealthDescription | fl
+}
 
 task_message "20-01-02" "List vSAN Datastore Space Usage"
 Get-Cluster $nest_cluster_name | Get-VsanSpaceUsage
