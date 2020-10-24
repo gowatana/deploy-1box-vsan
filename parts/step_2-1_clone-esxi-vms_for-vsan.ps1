2ec172be116b2bfd8925d556757a1c1c84f28689
Go Watanabe <gowatana@vmtn.jp>
2020-10-24 19:24:24 +0900

    add ResourcePool setting

--- parts/step_2-1_clone-esxi-vms_for-vsan.ps1
+++ parts/step_2-1_clone-esxi-vms_for-vsan.ps1
@@ -10,12 +10,29 @@ $esxi_vm_folder = Get-Datacenter $base_dc_name | Get-Folder -Type VM -Name "vm" 
     New-Folder -Name $esxi_vm_folder_name -ErrorAction:Ignore
 $esxi_vm_folder | select Name
 
+task_message "02-01-00b" "Create ResourcePool"
+if($base_rp_name){
+    #$base_rp = "RP-" + $nest_vc_address + "_" + $nest_cluster_name
+    Get-Datacenter $base_dc_name | Get-Cluster -Name $base_cluster_name | Get-ResourcePool -Name "Resources" |
+        New-ResourcePool -Name $base_rp_name -ErrorAction:Ignore | Out-Null
+    $base_rp = Get-Datacenter $base_dc_name | Get-Cluster -Name $base_cluster_name | Get-ResourcePool -Name "Resources" |
+        Get-ResourcePool -Name $base_rp_name
+    $base_rp | select Name
+}else{
+    "Skip"
+}
+
 # Clone Nested ESXi VMs
 $vm_name_list | ForEach-Object {
     $vm_name = $_
 
     task_message "02-01-01" ("Clone VM: " + $vm_name)
-    $vm = New-VM -VM $template_vm_name -Name $vm_name -VMHost (Get-VMHost $base_hv_name) -Location $esxi_vm_folder -Datastore $base_ds_name -StorageFormat Thin -ErrorAction:Stop
+    if($base_rp){
+        $vm = New-VM -VM $template_vm_name -Name $vm_name -ResourcePool $base_rp -Location $esxi_vm_folder -Datastore $base_ds_name -StorageFormat Thin -ErrorAction:Stop
+    }else{
+        $vm = New-VM -VM $template_vm_name -Name $vm_name -VMHost (Get-VMHost $base_hv_name) -Location $esxi_vm_folder -Datastore $base_ds_name -StorageFormat Thin -ErrorAction:Stop
+    }
+    
     $vm | select Name,NumCpu,MemoryGB,Folder,VMHost,HardwareVersion,GuestId | Format-List
 
     task_message "02-01-02" ("Set vNIC#1: " + $vm_name)
