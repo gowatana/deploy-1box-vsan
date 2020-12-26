e0e1bd73cb69173b5bc3ab0b11a8471c0864be5d
Go Watanabe <gowatana@vmtn.jp>
2020-12-27 01:48:58 +0900

    fix file name

--- setup_vSAN-Cluster.ps1
+++ setup_vSAN-Cluster.ps1
@@ -23,7 +23,7 @@ disconnect_all_vc
 task_message "Step-01" "Create vSphere Cluster"
 if($create_vsphre_cluster -eq $true){
     connect_vc -vc_addr $nest_vc_address -vc_user $nest_vc_user -vc_pass $nest_vc_pass
-    ./parts/step_1-1_create-vsphere-cluster.ps1
+    ./parts/step_01-1_create-vsphere-cluster.ps1
     disconnect_all_vc
 }else{
     "Skip"
@@ -32,8 +32,8 @@ if($create_vsphre_cluster -eq $true){
 task_message "Step-02" "Setup Base-vSphere"
 if($create_esxi_vms -eq $true){
     connect_vc -vc_addr $base_vc_address -vc_user $base_vc_user -vc_pass $base_vc_pass
-    ./parts/step_2-1_clone-esxi-vms_for-vsan.ps1
-    ./parts/step_2-2_config-esxi-guest_for-vsan.ps1
+    ./parts/step_02-1_clone-esxi-vms_for-vsan.ps1
+    ./parts/step_02-2_config-esxi-guest_for-vsan.ps1
     disconnect_all_vc
 }else{
     "Skip"
@@ -42,8 +42,8 @@ if($create_esxi_vms -eq $true){
 task_message "Step-03" "Setup Nested-vSphere"
 if($create_vsphre_cluster -eq $true){
     connect_vc -vc_addr $nest_vc_address -vc_user $nest_vc_user -vc_pass $nest_vc_pass
-    ./parts/step_3-1_setup-vsphere-cluster.ps1
-    ./parts/step_3-2_create-vmk-port_on-vss.ps1
+    ./parts/step_03-1_setup-vsphere-cluster.ps1
+    ./parts/step_03-2_create-vmk-port_on-vss.ps1
     disconnect_all_vc
 }else{
     "Skip"
@@ -52,15 +52,15 @@ if($create_vsphre_cluster -eq $true){
 task_message "Step-04" "Setup vDS"
 if($create_vds -eq $true){
     connect_vc -vc_addr $nest_vc_address -vc_user $nest_vc_user -vc_pass $nest_vc_pass
-    ./parts/step_4-1_create-vds.ps1
+    ./parts/step_04-1_create-vds.ps1
     disconnect_all_vc
 }
 
 task_message "Step-05" "Setup Witness-Host VA"
 if($create_witness_vm -eq $true){
     connect_vc -vc_addr $base_vc_address -vc_user $base_vc_user -vc_pass $base_vc_pass
-    ./parts/step_5-1_clone-vSAN-Witness-VA.ps1
-    ./parts/step_5-2_config-vSAN-Witness-VA-Guest.ps1
+    ./parts/step_05-1_clone-vSAN-Witness-VA.ps1
+    ./parts/step_05-2_config-vSAN-Witness-VA-Guest.ps1
     disconnect_all_vc
 }else{
     "Skip"
@@ -69,7 +69,7 @@ if($create_witness_vm -eq $true){
 task_message "Step-06" "Setup Witness-Host on vCenter"
 if($create_witness_vm -eq $true){
     connect_vc -vc_addr $nest_vc_address -vc_user $nest_vc_user -vc_pass $nest_vc_pass
-    ./parts/step_6-1_add-vSAN-Witness-Host-WTS.ps1
+    ./parts/step_06-1_add-vSAN-Witness-Host-WTS.ps1
     disconnect_all_vc
 }else{
     "Skip"
@@ -78,7 +78,7 @@ if($create_witness_vm -eq $true){
 task_message "Step-07" "Setup Data-Host for vSAN WTS"
 if($create_vsan_wts -eq $true){
     connect_vc -vc_addr $nest_vc_address -vc_user $nest_vc_user -vc_pass $nest_vc_pass
-    ./parts/step_7-1_setup-vsan-witness-nw.ps1
+    ./parts/step_07-1_setup-vsan-witness-nw.ps1
     disconnect_all_vc
 }else{
     "Skip"
@@ -87,8 +87,8 @@ if($create_vsan_wts -eq $true){
 task_message "Step-08" "Setup vSAN Cluster"
 if($create_vsan_cluster -eq $true){
     connect_vc -vc_addr $nest_vc_address -vc_user $nest_vc_user -vc_pass $nest_vc_pass
-    ./parts/step_8-1_setup-vsan-disk.ps1
-    ./parts/step_8-2_setup-vsan-cluster.ps1
+    ./parts/step_08-1_setup-vsan-disk.ps1
+    ./parts/step_08-2_setup-vsan-cluster.ps1
     disconnect_all_vc
 }else{
     "Skip"
@@ -98,7 +98,7 @@ task_message "Step-09" "Add VMDK for vSAN Disk Group"
 if($vsan_dg_count){
     connect_vc -vc_addr $base_vc_address -vc_user $base_vc_user -vc_pass $base_vc_pass
     for($i=2; $i -le $vsan_dg_count; $i++){
-        ./parts/step_9-1_add-vsan-vmdk.ps1
+        ./parts/step_09-1_add-vsan-vmdk.ps1
     }
     disconnect_all_vc
 }else{
