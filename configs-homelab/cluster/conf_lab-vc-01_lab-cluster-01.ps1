c91417f0afd902591a05bc8a0dbdf4aa869ed35e
Go Watanabe <gowatana@vmtn.jp>
2021-01-22 01:51:10 +0900

    update

--- configs-homelab/cluster/conf_lab-vc-01_lab-cluster-01.ps1
+++ configs-homelab/cluster/conf_lab-vc-01_lab-cluster-01.ps1
@@ -4,9 +4,10 @@
 # Setup flags
 $create_esxi_vms       = $true  # $true or $false
 $create_vsphre_cluster = $true  # $true or $false
+$create_vds            = $true  # $true or $false
 $create_witness_vm     = $false # $true or $false
-$create_vsan_wts       = $false # $true or $false
-$create_vsan_cluster   = $false # $true or $false
+$setup_vsan_wts        = $false # $true or $false (WTS: Witness Traffic Separation)
+$create_vsan_cluster   = $true  # $true or $false
 $create_vsan_2node     = $false # $true or $false
 
 # ----------------------------------------
@@ -22,7 +23,7 @@ $base_rp_name = "rp-05-nested-lab"
 # vSAN Cluster settings
 
 $nest_dc_name = "lab-dc-01"
-$nest_cluster_name = "Cluster-01"
+$nest_cluster_name = "lab-cluster-01"
 $vm_num = 3
 $hv_ip_4oct_start = 31 # 4th Octet for ESXi-vmk0-IP
 
@@ -30,14 +31,15 @@ $hv_ip_4oct_start = 31 # 4th Octet for ESXi-vmk0-IP
 # Nested ESXi settings
 
 # ESXi Template VM
-$template_vm_name = "esxi70-template-02"
+$template_vm_name = "esxi70u1c-template-01"
+$linked_clone = $true
 
 # VM Name / ESXi Hostname Prefix
-$vm_name_prefix = "vm-esxi-a-"
-$nest_hv_hostname_prefix = "esxi-a-"
+$vm_name_prefix = "vm-lab-esxi-"
+$nest_hv_hostname_prefix = "lab-esxi-"
 
 # ESXi Data host Spec
-$esxi_memory_gb = 8
+$esxi_memory_gb = 12
 
 # Nested ESXi User / Password
 $hv_user = "root"
@@ -62,7 +64,7 @@ $add_vmk2 = $true # $true or $false
 
 $vmotion_vmk_port = "vmk1"
 $vsan_vmk_port = "vmk2"
-$witness_vmk_port = "" # vSAN WTS only
+$witness_vmk_port = "vmk0" # vSAN WTS only
 
 $vmk1_vss = "vSwitch0"
 $vmk1_pg = "pg_vmk_vmotion"
@@ -76,22 +78,11 @@ $vmk2_vlan = 1002
 $vmk2_ip_prefix = "10.0.2." # $hv_ip_prefix_vmk2 + $hv_ip_4oct_start => 10.0.2.31
 $vmk2_subnetmask = "255.255.255.0" # /24
 
+$multi_vmnic = 4 # add vmnic1 .. vmnic3
+
 # ----------------------------------------
 # vDS Settings
-
-$create_vds = $false
-<#
-    $vds_name = "vds-01"
-
-    $vds_mgmt_pg_name = "dvpg_" + $vds_name + "_mgmt"
-    $vds_mgmt_pg_vlan = 0
-    $vds_vmotion_pg_name = "dvpg_" + $vds_name + "_vmotion"
-    $vds_vmotion_pg_vlan = 1001
-    $vds_vsan_pg_name = "dvpg_" + $vds_name + "_vsan"
-    $vds_vsan_pg_vlan = 1002
-    $vds_guest_pg_name = "dvpg_" + $vds_name + "_guest"
-    $vds_guest_pg_vlan = 0
-#>
+$vds_config = (Split-Path -Path $PSScriptRoot -Parent) + "/vds/conf_lab-vds-01.ps1"
 
 # ----------------------------------------
 # Storage Settings
@@ -110,25 +101,4 @@ $vsan_dg_count = 1 # Multi-Diskgroup setup
 
 # ----------------------------------------
 # vSAN Witness Config
-
-<#
-    # Witness VA Base Config
-    $base_witness_pg_name_1 = "Nested-Trunk-Network"
-    $base_witness_pg_name_2 = "Nested-Trunk-Network"
-
-    # Witness Host Config
-    $witness_dc = "LAB-DC"
-    $witness_host_folder = "Witness-Hosts" # if "host", it is added to DC
-    $vsan_witness_host_name = "esxi-038"
-    $vsan_witness_host_domain = "go-lab.jp"
-    $vsan_witness_host_ip = "192.168.1.38"
-    $vsan_witness_host_subnetmask = "255.255.255.0"
-    $vsan_witness_host_gw = "192.168.1.1"
-    $vsan_witness_dns_1 = "192.168.1.101"
-    $vsan_witness_dns_2 = "192.168.1.102"
-    $vsan_witness_host_vcname = $vsan_witness_host_ip
-
-    $vsan_wts = $false # Witness Traffic Separation (WTS): $true or $false
-    $vsan_witness_template_name = "VMware-VirtualSAN-Witness-6.7.0.update03-14320388"
-    $vsan_witness_va_name = "vm-esxi-witness-" + $vsan_witness_host_ip
-#>
+$witness_config = (Split-Path -Path $PSScriptRoot -Parent) + "/witness/conf_Witness-VA_X.ps1"
