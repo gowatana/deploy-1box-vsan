400dd1efb5fa15279dbce6c13f5f3c40603c889c
Go Watanabe <gowatana@vmtn.jp>
2022-02-26 02:14:44 +0900

    add new-config lab-cluster-01 on lab-vc-01

--- lab_setup.ps1
+++ lab_setup.ps1
@@ -3,18 +3,45 @@ $operation_type = $args[1]
 
 # Load vSAN-Lab config file.
 ls $config_file | Out-Null
-if($? -eq $false){"vSAN-Lab config file not found."; exit}
+if($? -eq $false){"Lab config file not found."; exit}
 . $config_file
 
-# Load additional config files
+"Load Base-vCenter Configs"
+Get-Item $base_vc_config -ErrorAction:Stop | fl FullName,LastWriteTime
+. $base_vc_config
+$base_vc_address = $vc_address
+$base_vc_user = $vc_user
+$base_vc_pass = $vc_pass
+
+"Load Nest-vCenter Configs"
+Get-Item $nest_vc_config -ErrorAction:Stop | fl FullName,LastWriteTime
+. $nest_vc_config
+$nest_vc_address = $vc_address
+$nest_vc_user = $vc_user
+$nest_vc_pass = $vc_pass
+
+"Load Base-vSphere Config file:"
+Get-Item $base_env_config -ErrorAction:Stop | fl FullName,LastWriteTime
+. $base_env_config
+
+"Load Nest-vSphere Config file:"
+Get-Item $nest_env_config -ErrorAction:Stop | fl FullName,LastWriteTime
+. $nest_env_config
+
+"Load vDS-vSphere Config file:"
 if($create_vds -eq $true){
     Get-Item $vds_config -ErrorAction:Stop | fl FullName,LastWriteTime
     . $vds_config
+}else{
+    "Skip"
 }
 
-if($create_witness_vm -eq $true){
+"Load vSAN-Witness-VA Config file:"
+if($create_witness_vm -eq $true){   
     Get-Item $witness_config -ErrorAction:Stop | fl FullName,LastWriteTime
     . $witness_config
+}else{
+    "Skip"
 }
 
 if($operation_type -eq "pretest"){
---
+++ labs/01_vcenter-login/infra-vc-01.ps1
@@ -0,0 +1,4 @@
+# vCenter Login info
+$vc_address = "infra-vc-01.go-lab.jp"
+$vc_user = "automation@go-lab.jp"
+$vc_pass = "VMware1!"
